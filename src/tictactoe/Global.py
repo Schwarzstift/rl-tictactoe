ab038c244cdb0138ed2b14a9fdebda716e411a3d
cars_ch <chris.carstensen@dlr.de>
2017-01-30 12:30:20 +0100

    Refactor even more

---
+++ src/tictactoe/Global.py
@@ -0,0 +1,32 @@
+
+BOARD_FORMAT = "----------------------------\n| {0} | {1} | {2} |\n|--------------------------|\n| {3} | {4} | {5} |\n|--------------------------|\n| {6} | {7} | {8} |\n----------------------------"
+NAMES = [' ', 'X', 'O']
+
+EMPTY = 0
+PLAYER_X = 1
+PLAYER_O = 2
+DRAW = 3
+
+
+
+def empty_state():
+    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
+
+
+def game_over(state):
+    # noinspection PyShadowingNames
+    for i in range(3):
+        if state[i][0] != EMPTY and state[i][0] == state[i][1] and state[i][0] == state[i][2]:
+            return state[i][0]
+        if state[0][i] != EMPTY and state[0][i] == state[1][i] and state[0][i] == state[2][i]:
+            return state[0][i]
+    if state[0][0] != EMPTY and state[0][0] == state[1][1] and state[0][0] == state[2][2]:
+        return state[0][0]
+    if state[0][2] != EMPTY and state[0][2] == state[1][1] and state[0][2] == state[2][0]:
+        return state[0][2]
+    # noinspection PyShadowingNames
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] == EMPTY:
+                return EMPTY
+    return DRAW
