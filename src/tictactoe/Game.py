3b488015de32164715ee14f7a8ff256888d0d5b6
Schwarzstift <chris.carstensen@googlemail.com>
2017-01-30 17:55:28 +0100

    Finished multiple games average

--- src/tictactoe/Game.py
+++ src/tictactoe/Game.py
@@ -26,6 +26,7 @@ import csv
 
 import copy
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from tictactoe.Agent import Agent
 from tictactoe.Human import Human
@@ -63,26 +64,43 @@ def draw_results(win_counter):
     fig.show()
 
 
+def draw_average_results(player_o_wins, player_x_wins, draw):
+    results = [player_o_wins, player_x_wins, draw]
+    labels = ["Player O Win", "Player X Win", "Draw"]
+    plottingData = pd.DataFrame()
+    for result,name in zip(results, labels):
+        df = pd.DataFrame(result).transpose()
+        df = df.mean(axis=1)
+        plottingData[name] = df
+
+    plottingData.plot()
+
+
 def average_training_agents(agent1, agent2):
-    average_size = 1000
+    average_size = 100
+
     player_o_win = []
     player_x_win = []
     draw = []
+
     for i in range(average_size):
+        if i % (average_size/25) == 0:
+            print "Process: " + str(round((i/float(average_size))*100)) + "%"
+
         copy_of_agent1 = copy.deepcopy(agent1)
         copy_of_agent2 = copy.deepcopy(agent2)
         result = train_agents_against_each_other(copy_of_agent1, copy_of_agent2, False)
+
         player_o_win.append(result[PLAYER_O])
         player_x_win.append(result[PLAYER_X])
         draw.append(result[DRAW])
+    draw_average_results(player_o_win, player_x_win, draw)
 
 
 def train_agents_against_each_other(agent1, agent2, draw_temp_results):
     win_counter = {PLAYER_X:[0], PLAYER_O:[0], DRAW:[0]}
     games = 3000
     for i in range(games):
-        if i % (games/25) == 0:
-            print "Process: " + str(round((i/float(games))*100)) + "%"
 
         winner = play(agent1, agent2)
         agent1.episode_over(winner)
@@ -110,7 +128,9 @@ def game_loop_computer_vs_human():
 if __name__ == "__main__":
     p1 = Agent(PLAYER_X, loss_val=-1)
     p2 = Agent(PLAYER_O, loss_val=-1)
+    p2.epsilon=0.1
     series_each_other = ['P1-Win', 'P2-Win', 'Draw']
-    train_agents_against_each_other()
+    average_training_agents(p1, p2)
+    #train_agents_against_each_other(p1,p2,True)
     plt.show()
     # game_loop_computer_vs_human()
